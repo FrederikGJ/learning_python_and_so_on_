3f77bf4e3313565183bbb480342afd70ab060c5c
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-06-17 12:57:25 +0200

    change to aviod duplicates

--- scraper.py
+++ scraper.py
@@ -5,7 +5,13 @@ page_to_scrape = requests.get("https://www.dr.dk/nyheder/penge")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 links = soup.findAll(href=lambda href: href and href.startswith('/nyheder/'))
 
-for link in links:
-    print(link.text)
+printed_texts = set()  
 
+for link in links:
+    text = link.text
+
+    if text not in printed_texts:
+        print(text)
+        print("")
+        printed_texts.add(text)
 
