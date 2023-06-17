b2888f45c25d0819f50a59211aac7c3e1eb4a4d0
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-06-17 12:05:38 +0200

    all the files have changed lolo

---
+++ cars.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, make, model, color, sound, name):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.sound = sound
+        self.name = name
+    def car_broke_down(self):
+            print("your car named " + self.name + " broke tha fuck down!!!! :-O ")
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from cars import Car
+
+car1 = Car("Toyota", "Yaris", "Greeeen", "Wroooomomoooom", "Henriks dytter")
+
+print("My car has the color "+car1.color + "...")
+car1.car_broke_down()
+print("now what do i do?? ")
+print("you get a new " + car1.model)
