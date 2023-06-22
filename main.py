17cb1634bd6448ef3015046bf340d4eaf5b26eb1
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-06-22 20:38:24 +0200

    opdateret

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-
+from tv2_business_scraper import TV2Scraper
 
 class Main:
     def __init__(self):
-        print("lol")
+        scraper = TV2Scraper()
+        print("Antallet af positive ord er: ", scraper.count_positive_words())
+        print("Antallet af negative ord er: ", scraper.count_negative_words())
+        scraper.print_labels()
+
+if __name__ == '__main__':
+    Main()
--- tv2_business_scraper.py
+++ tv2_business_scraper.py
@@ -1,40 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 
-class tv2_scraper: 
+class TV2Scraper:   
+    def __init__(self):
+        self.page_to_scrape = requests.get("https://nyheder.tv2.dk/business")
+        self.soup = BeautifulSoup(self.page_to_scrape.text, "html.parser")
+        self.elements = self.soup.find_all(lambda tag: tag.name == 'a' and tag.get('href') and tag.get('href').startswith('https://nyheder.tv2.dk/business/'))
+        self.aria_labels = set()  
+        for element in self.elements:
+            aria_label = element.get('aria-label')
+            if aria_label and aria_label not in self.aria_labels:
+                self.aria_labels.add(aria_label)
+        self.positive_words = ["godt", "opsving", "stærkt", "gode", "sejr", "vækst"]
+        self.negative_words = ["inflation", "konflikt", "inflation", "prisstigninger","nedtur", "strejke", "bedrageri"]
 
-    def count_positive_words(texts, words):
+    def count_positive_words(self):
         count = 0
-        for text in texts:
-            for word in words:
-                count += text.lower().count(word)
+        for label in self.aria_labels:
+            for word in self.positive_words:
+                count += label.lower().count(word)
         return count
 
-    def count_negative_words(texts, words):
+    def count_negative_words(self):
         count = 0
-        for text in texts:
-            for word in words:
-                count += text.lower().count(word)
+        for label in self.aria_labels:
+            for word in self.negative_words:
+                count += label.lower().count(word)
         return count
 
-    page_to_scrape = requests.get("https://nyheder.tv2.dk/business")
-    soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-    elements = soup.find_all(lambda tag: tag.name == 'a' and tag.get('href') and tag.get('href').startswith('https://nyheder.tv2.dk/business/'))
+    def print_labels(self):
+        for label in self.aria_labels:
+            print(label)
 
-    aria_labels = set()  
-
-    for element in elements:
-        aria_label = element.get('aria-label')
-        if aria_label and aria_label not in aria_labels:
-            aria_labels.add(aria_label)
-
-    positive_words = ["godt", "opsving", "stærkt", "gode", "sejr", "vækst"]
-    positive_ord = count_positive_words(aria_labels, positive_words)
-    print("Antallet af positive ord er: ", positive_ord)
-
-    negative_words = ["inflation", "konflikt", "inflation", "prisstigninger","nedtur", "strejke", "bedrageri"]
-    negative_ord = count_negative_words(aria_labels, negative_words)
-    print("Antallet af negative ord er:", negative_ord)
-
-    for label in aria_labels:
-        print(label)
