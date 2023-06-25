349bcb35cf1aa9df0253c42e19953662a67a5d97
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-06-25 23:13:41 +0200

    lopp

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from main import Main
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def home(): 
     main = Main()
+    
     total_negative = main.total_negative
     total_positive = main.total_positive
 
     return render_template('index.html', total_negative=total_negative, total_positive=total_positive)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
 
--- main.py
+++ main.py
@@ -13,26 +13,6 @@ class Main:
         self.total_negative = scraper1.count_negative_words() + scraper2.count_negative_words() + scraper3.count_negative_words() + scraper4.count_negative_words()
         self.total_positive = scraper1.count_positive_words() + scraper2.count_positive_words() + scraper3.count_positive_words() + scraper4.count_positive_words()
 
-        #print("Antallet af positive ord hos tv2 er: ", scraper1.count_positive_words())
-        #print("Antallet af negative ord hos tv2 er: ", scraper1.count_negative_words())
-
-        #print("Antallet af positive ord hos Borsen er: ", scraper2.count_positive_words())
-        #print("Antallet af negative ord hos Borsen er: ", scraper2.count_negative_words())
-        
-        #print("Antallet af positive ord hos Finans er: ", scraper3.count_positive_words())
-        #print("Antallet af negative ord hos Finans er: ", scraper3.count_negative_words())
-        
-        #print("Antallet af positive ord hos Danmarks Radio er: ", scraper4.count_positive_words())
-        #print("Antallet af negative ord hos Danmarks Radio er: ", scraper4.count_negative_words())
-        
-        #print("\nOverskrifter fra TV2 :\n")
-        #scraper1.print_texts()
-        #print("\nOverskrifter fra Borsen :\n")
-        #scraper2.print_texts()
-        #print("\nOverskrifter fra Finans :\n")
-        #scraper3.print_texts()
-        #print("\nOverskrifter fra Danmarks Radio :\n")
-        #scraper4.print_texts()
 
 if __name__ == '__main__':
     Main()
