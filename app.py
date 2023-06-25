64e9ff1fe851a0ae6bd25910e5a004b27b860638
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-06-25 22:01:53 +0200

    changes to app

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Create the Flask application
 app = Flask(__name__)
 
 # Define a route and a function to handle it
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def hello(): 
+   return render_template('index.html')
 
 # Run the application
 if __name__ == '__main__':
