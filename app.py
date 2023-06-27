b5f05325c958823c491a2444103d09650929a6f6
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-06-27 23:17:22 +0200

    updated site

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
 from main import Main
+import pandas as pd 
+import plotly
+import plotly.express as px
+import json
+
 
 app = Flask(__name__)
 
@@ -10,8 +15,15 @@ def home():
     total_negative = main.total_negative
     total_positive = main.total_positive
 
-    return render_template('index.html', total_negative=total_negative, total_positive=total_positive)
+    total_word = [["Negative ord", total_negative],["Postitive ord", total_positive]]
+
+    data_frame = pd.DataFrame(total_word, columns=['Type', 'Count'])
+
+    figure = px.bar(data_frame, x='Type', y='Count', barmode='group')
+    
+    graphJSON = json.dumps(figure, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.html', total_negative=total_negative, total_positive=total_positive, graphJSON=graphJSON)
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
